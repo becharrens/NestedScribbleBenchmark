79b872173a6a4961eeac6eba21414efd65fd5701
becharrens <bes.seb.98@gmail.com>
2021-01-07 16:15:24 +0100

    Add waitgroup to base implementation

--- primesieve_base/bounded_sieve.go
+++ primesieve_base/bounded_sieve.go
@@ -1,5 +1,7 @@
 package primesieve_base
 
+import "sync"
+
 // Send the sequence 2, 3, 4, ... to channel 'ch'.
 // func GeneratePrimes(n int, ch chan<- int, doneChan chan<- bool) {
 // 	for i := 2; i <= n; i++ {
@@ -58,15 +60,18 @@ package primesieve_base
 // 	return primes
 // }
 
-var NumChannels = 0
+func StartMain(wg *sync.WaitGroup, primes *[]int, n int, sendInt chan int, recvPrimes chan int, morePrimes chan bool) {
+	defer wg.Done()
+	sendInt <- n
+	sendInt <- 2
+	Main(primes, recvPrimes, morePrimes)
+}
 
-func Main(primes []int, recvPrimes chan int, morePrimes chan bool) []int {
+func Main(primes *[]int, recvPrimes chan int, morePrimes chan bool) {
 	if <-morePrimes {
 		prime := <-recvPrimes
-		primes := append(primes, prime)
-		return Main(primes, recvPrimes, morePrimes)
-	} else {
-		return primes
+		*primes = append(*primes, prime)
+		Main(primes, recvPrimes, morePrimes)
 	}
 }
 
@@ -76,25 +81,30 @@ func PrimeSieve(n int) []int {
 	}
 	sendInt := make(chan int, 1)
 	recvPrimes := make(chan int, 1)
-	// filterPrime := make(chan int, 1)
 	morePrimes := make(chan bool, 1)
-	NumChannels += 3
 
-	go FirstWorker(sendInt, recvPrimes, morePrimes)
-	sendInt <- n
-	sendInt <- 2
 	primes := []int{2}
-	return Main(primes, recvPrimes, morePrimes)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go FirstWorker(&wg, sendInt, recvPrimes, morePrimes)
+	go StartMain(&wg, &primes, n, sendInt, recvPrimes, morePrimes)
+
+	wg.Wait()
+	return primes
 }
 
-func FirstWorker(intChan <-chan int, resChan chan<- int, morePrimes chan<- bool) {
+func FirstWorker(wg *sync.WaitGroup, intChan <-chan int, resChan chan<- int, morePrimes chan<- bool) {
+	defer wg.Done()
 	ubound := <-intChan
 	firstPrime := <-intChan
 	primes := genPrimes(ubound, firstPrime)
-	SieveChoice(primes, resChan, morePrimes)
+	SieveChoice(wg, primes, resChan, morePrimes)
 }
 
-func SieveWorker(primeChan, primesIn <-chan int, resChan chan<- int, morePrimes chan<- bool) {
+func SieveWorker(wg *sync.WaitGroup, primeChan, primesIn <-chan int, resChan chan<- int, morePrimes chan<- bool) {
+	defer wg.Done()
 	filterPrime := <-primeChan
 	var primes []int
 	for prime := range primesIn {
@@ -102,10 +112,10 @@ func SieveWorker(primeChan, primesIn <-chan int, resChan chan<- int, morePrimes 
 			primes = append(primes, prime)
 		}
 	}
-	SieveChoice(primes, resChan, morePrimes)
+	SieveChoice(wg, primes, resChan, morePrimes)
 }
 
-func SieveChoice(primes []int, resChan chan<- int, morePrimes chan<- bool) {
+func SieveChoice(wg *sync.WaitGroup, primes []int, resChan chan<- int, morePrimes chan<- bool) {
 	if len(primes) == 0 {
 		morePrimes <- false
 	} else {
@@ -113,8 +123,8 @@ func SieveChoice(primes []int, resChan chan<- int, morePrimes chan<- bool) {
 		resChan <- primes[0]
 		filterPrimeChan := make(chan int, 1)
 		sendPrimes := make(chan int, 1)
-		NumChannels += 2
-		go SieveWorker(filterPrimeChan, sendPrimes, resChan, morePrimes)
+		wg.Add(1)
+		go SieveWorker(wg, filterPrimeChan, sendPrimes, resChan, morePrimes)
 		forwardPrimes(primes, filterPrimeChan, sendPrimes)
 	}
 }
@@ -136,3 +146,14 @@ func forwardPrimes(primes []int, primeChan, sendPrimes chan<- int) {
 	}
 	close(sendPrimes)
 }
+
+// 100 - Scribble: 305, 178.750591, Base: 113, 31.324126, Ratio: 0.370492
+// 1100 - Scribble: 7668, 452.370821, Base: 4169, 288.037503, Ratio: 0.543688
+// 2100 - Scribble: 21332, 1071.216199, Base: 11990, 517.326917, Ratio: 0.562066
+// 3100 - Scribble: 40331, 1056.366749, Base: 22780, 632.720595, Ratio: 0.564826
+// 4100 - Scribble: 65228, 1956.849161, Base: 36788, 779.933996, Ratio: 0.563991
+// 5100 - Scribble: 93846, 2259.329353, Base: 54142, 3312.552018, Ratio: 0.576924
+// 6100 - Scribble: 126499, 2662.512696, Base: 72355, 1267.370471, Ratio: 0.571981
+// 7100 - Scribble: 164252, 3041.754332, Base: 94528, 2959.454203, Ratio: 0.575506
+// 8100 - Scribble: 203316, 1618.411284, Base: 117686, 2019.867681, Ratio: 0.578833
+// 9100 - Scribble: 249860, 2836.200837, Base: 144499, 4472.291520, Ratio: 0.578320
