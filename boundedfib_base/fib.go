b0932408605ac9a6281c2567011b3bb046fde21e
becharrens <bes.seb.98@gmail.com>
2021-01-07 16:24:39 +0100

    Add waitgroup to implementation

--- boundedfib_base/fib.go
+++ boundedfib_base/fib.go
@@ -1,14 +1,25 @@
 package boundedfib_base
 
+import "sync"
+
+func startFstSender(wg *sync.WaitGroup, fib, idx, ubound int, sendChan chan int) {
+	defer wg.Done()
+	fstSender(fib, idx, ubound, sendChan)
+}
+
 func fstSender(fib, idx, ubound int, sendChan chan int) {
 	sendChan <- ubound
 	sendChan <- idx
 	sendChan <- fib
+}
 
+func startSndSender(wg *sync.WaitGroup, fib, idx, ubound int, sendChan1, sendChan2 chan int, stopChan chan bool) {
+	defer wg.Done()
+	sndSender(fib, idx, ubound, sendChan1, sendChan2, stopChan)
 }
 
 func sndSender(fib, idx, ubound int, sendChan1, sendChan2 chan int, stopChan chan bool) {
-	sendChan1 <- idx
+	// sendChan1 <- idx
 	sendChan1 <- fib
 	stop := <-stopChan
 	if !stop {
@@ -16,13 +27,13 @@ func sndSender(fib, idx, ubound int, sendChan1, sendChan2 chan int, stopChan cha
 	}
 }
 
-func fibCalc(fib1Chan, fib2Chan, sendChan1, sendChan2, nextFib1Chan, resChan chan int,
+func fibCalc(wg *sync.WaitGroup, fib1Chan, fib2Chan, sendChan1, sendChan2, nextFib1Chan, resChan chan int,
 	prevStopChan, stopChan chan bool) {
+	defer wg.Done()
 	ubound := <-fib1Chan
 	idx1 := <-fib1Chan
 	fib1 := <-fib1Chan
 
-	_ = <-fib2Chan
 	fib2 := <-fib2Chan
 
 	fib := fib1 + fib2
@@ -35,7 +46,8 @@ func fibCalc(fib1Chan, fib2Chan, sendChan1, sendChan2, nextFib1Chan, resChan cha
 		newFibChan1 := make(chan int, 1)
 		newFibChan2 := make(chan int, 1)
 		nextStopChan := make(chan bool, 1)
-		go fibCalc(nextFib1Chan, sendChan1, newFibChan1, newFibChan2, sendChan2, resChan, stopChan, nextStopChan)
+		wg.Add(1)
+		go fibCalc(wg, nextFib1Chan, sendChan1, newFibChan1, newFibChan2, sendChan2, resChan, stopChan, nextStopChan)
 		sndSender(fib, idx, ubound, sendChan1, sendChan2, stopChan)
 	}
 }
@@ -53,10 +65,12 @@ func Fibonacci(n int) int {
 	nextFibChan1 := make(chan int, 1)
 	nextFibChan2 := make(chan int, 1)
 	nextStopChan := make(chan bool, 1)
+	var wg sync.WaitGroup
+	wg.Add(3)
 
-	go fstSender(1, 1, n, fstChan)
-	go sndSender(1, 2, n, sndChan1, sndChan2, stopChan)
-	go fibCalc(fstChan, sndChan1, nextFibChan1, nextFibChan2, sndChan2, resChan, stopChan, nextStopChan)
+	go startFstSender(&wg, 1, 1, n, fstChan)
+	go startSndSender(&wg, 1, 2, n, sndChan1, sndChan2, stopChan)
+	go fibCalc(&wg, fstChan, sndChan1, nextFibChan1, nextFibChan2, sndChan2, resChan, stopChan, nextStopChan)
 	// for i := 3; i <= n; i++ {
 	// 	nextFibChan1 := make(chan int, 1)
 	// 	nextFibChan2 := make(chan int, 1)
@@ -67,5 +81,7 @@ func Fibonacci(n int) int {
 	// 	sndChan1 = nextFibChan1
 	// 	sndChan2 = nextFibChan2
 	// }
-	return <-resChan
+	res := <-resChan
+	wg.Wait()
+	return res
 }
